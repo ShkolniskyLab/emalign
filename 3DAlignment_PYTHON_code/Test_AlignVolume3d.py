a0bef16b61d302b5314a4f8e9ed012bd857948fc
Yael <56630933+yaelharpaz1@users.noreply.github.com>
2022-02-16 16:56:43 +0200

    Add files via upload

--- 3DAlignment_PYTHON_code/Test_AlignVolume3d.py
+++ 3DAlignment_PYTHON_code/Test_AlignVolume3d.py
@@ -18,10 +18,12 @@ from reshift_vol import reshift_vol
 
 
 # Test for volume alignment
+#np.random.seed(1337)
+np.random.seed(2021)
 
 # Read molecule:
-vol = read_mrc('0825_C6.mrc')
-sym = 'C6'
+#vol = read_mrc('0825_C6.mrc')
+#sym = 'C6'
 
 #vol = read_mrc('10280_C1.mrc')
 #sym = 'C1'
@@ -32,34 +34,32 @@ sym = 'C6'
 #vol = read_mrc('4179_T.mrc')
 #sym = 'T'
 
-#vol = read_mrc('24494_I.mrc')
-#sym = 'I'
+vol = read_mrc('24494_I.mrc')
+sym = 'I'
 
 #vol = read_mrc('22658_O.mrc')
 #sym = 'O'
 
-
 out_shape = (129,129,129)
 vol = cryo_downsample(vol,out_shape)
 
-np.random.seed(1337)
+
 R_true = rand_rots(1).reshape((3,3))
 #R_true = mat_to_npy('trueR_test3d_forVol')
 
-vol_c = np.copy(vol)
-vol_rotated = fastrotate3d(vol_c, R_true)
-vol_rotated =  np.flip(vol_rotated, axis=2)  
-vol_rotated = reshift_vol(vol_rotated, np.array([-5, 0 ,0]))
+vol_rotated = fastrotate3d(vol.copy(), R_true)
+vol_rotated =  np.flip(vol_rotated.copy(), axis=2)  
+vol_rotated = reshift_vol(vol_rotated.copy(), np.array([-5, 0 ,0]))
 
 # Alignment algorithm:
 G = genSymGroup(sym)    
 class Struct:
     pass
 opt = Struct() 
-opt.sym = sym   
-opt.Nref = 30
+#opt.sym = sym   
+#opt.Nref = 30
 opt.G = G
-opt.downsample = 48
+#opt.downsample = 64
 opt.trueR = R_true
 
 bestR, bestdx, reflect, vol2aligned, bestcorr = AlignVolumes(vol,vol_rotated,1,opt)
