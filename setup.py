56006f1d9320c627a43607e554d19a85ef74eb8b
Yoel Shkolnisky <yoelsh@tauex.tau.ac.il>
2022-12-06 22:33:30 +0200

    Advance minor version.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(name='EMalign',
-      version='1.0.1',
+      version='1.0.2',
       description='EM align',
       long_description=long_description,
       long_description_content_type='text/markdown',
